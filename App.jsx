6cda6c210dfa7586b156dac916b9311f80ad1c3c
jlopez <jorney95@live.com>
2023-12-14 18:00:38 -0600

    add picker

--- App.jsx
+++ App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {View, ScrollView, Text, StyleSheet, Image} from 'react-native';
 import Header from './components/Header';
+import Formulario from './components/Formulario';
 
 function App() {
   return (
@@ -11,6 +12,9 @@ function App() {
           style={styles.imagen}
           source={require('./assets/img/cryptomonedas.png')}
         />
+        <View style={styles.contenido}>
+          <Formulario />
+        </View>
       </ScrollView>
     </>
   );
@@ -20,6 +24,9 @@ const styles = StyleSheet.create({
   imagen: {
     width: '100%',
     height: 150,
+    marginHorizontal: '2.5%',
+  },
+  contenido: {
     marginHorizontal: '2.5%',
   },
 });
--- components/Formulario.jsx
+++ components/Formulario.jsx
@@ -1,7 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import {Picker} from '@react-native-picker/picker';
 
 const Formulario = () => {
-  return <div>Formulario</div>;
+  const [moneda, setMoneda] = useState('');
+  const [cripto, setCripto] = useState('');
+
+  const obtenerMoneda = coin => {
+    setMoneda(coin);
+  };
+  return (
+    <View>
+      <Text style={styles.label}>Moneda</Text>
+      <Picker
+        selectedValue={moneda}
+        onValueChange={coin => obtenerMoneda(coin)}>
+        <Picker.Item label="- Selecione -" value="" />
+        <Picker.Item label="Dolar" value="USD" />
+        <Picker.Item label="Euro" value="EUR" />
+        <Picker.Item label="Colon" value="CRC" />
+      </Picker>
+      <Text style={styles.label}>Criptomoneda</Text>
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  label: {
+    fontFamily: 'Lato-Black.ttf',
+    fontSize: 22,
+    marginVertical: 20,
+    textTransform: 'uppercase',
+  },
+});
 
 export default Formulario;
